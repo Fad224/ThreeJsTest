b530cbe9f39e214baa0daac38d87cde282cd6d88
FadyAzzam <fad224@gmail.com>
2025-04-02 14:13:31 +0400

    WIP

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 // Scene setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-  75,
+  50,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
