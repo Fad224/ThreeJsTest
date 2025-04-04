0a9612dfca1694c29ddbbc10c4a4072c5bb5bc3f
FadyAzzam <fad224@gmail.com>
2025-04-04 17:47:11 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 /**
  * @type {import('vite').UserConfig}
  */
+
+
+// export default {
+//   base: process.env.NODE_ENV === 'production' ? '/ThreeJsTest/' : ''
+// }
+
 export default {
-  base: process.env.NODE_ENV === 'production' ? '/ThreeJsTest/' : ''
+  base: '/ThreeJsTest/',
 }
